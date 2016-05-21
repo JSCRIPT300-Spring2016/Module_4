a09af4c7d35a1cdaef74d160a6cecfb8e3950db1
Andrew Reid <Andrew Reid>
2016-05-21 13:01:55 -0700

    fixed after homework feedback

--- index.js
+++ index.js
@@ -49,18 +49,25 @@ app.get('/trucks', function (request, response, next) {
 // `/trucks/:name` This route returns a single truck object that matches
 // the name parameter passed in the route.
 // http://127.0.0.1:3000/trucks:Fez
-app.get('/trucks:name', function (request, response, next) {
-  var truckReq = request.params.name;
-  console.log('request[' + truckReq + ']');
-  var truck = trucks.getTruck(truckReq);
-  response.send(truck);
-  next();
+app.get('/trucks/:name', function (request, response, next) {
+    var truckReq = request.params.name;
+    console.log('request[' + truckReq + ']');
+    var truck = trucks.getTruck(truckReq);
+
+    //var foodTruck = truckReq[0].toUpperCase + truckReq.slice(1).toLowerCase();
+
+    if (!truck) {
+        response.status(404).json('No food type found for ' + truck)
+    } else {
+        response.send(truck);
+    }
+    next();
 });
 
 //  `/food-types` This route returns the list of all possible food types
 // served by trucks in the module
 // http://127.0.0.1:3000/foodTypes
-app.get('/foodTypes', function (request, response, next) {
+app.get('/food-types', function (request, response, next) {
   var foodList = trucks.getFoodTypes();
   response.send(foodList);
   next();
@@ -70,16 +77,22 @@ app.get('/foodTypes', function (request, response, next) {
 // food type that matches (case insensitive) the type parameter passed.
 // http://127.0.0.1:3000/foodTypes:Pizza
 // http://127.0.0.1:3000/foodTypes:pizza
-app.get('/foodTypes:type', function (request, response, next) {
+app.get('/food-types/:type', function (request, response, next) {
   var typeReq = request.params.type;
   console.log('request[' + typeReq + ']');
   var type = trucks.filterTrucksByFoodType(typeReq);
-  response.send(type);
+  if (!type) {
+    response.status(404).json('No food type found for ' + type)
+  } else {
+    response.send(type);
+  }
+  
   next();
 });
 
 // test for var openTrucks = require('./trucks');
 app.get('/openTrucks', function (request, response) {
+  var openTruck = trucks.filterTrucksByDay('Saturday');
   response.send(openTruck);
 });
 
