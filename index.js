3b6f6c5d56aee8aaeafaeec17c32b81a18134d10
Josh Smith <joshuap.smith@gmail.com>
2016-05-23 19:59:58 -0500

    Fixed typos
    Still can't get the food-types/:type route to work

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ app.get('/trucks', function (request, response) {
   var truckList = trucks.getTrucks();
 
   response.send(truckList);
-})
+});
 
 app.get('/trucks/:name', function(request, response) {
   var truck = trucks.getTruck(request.params.name);
 
   response.send(truck);
-})
+});
 
 app.get('/food-types', function(request, response) {
   var foodList = trucks.getFoodTypes();
 
   response.send(foodList);
-})
+});
 
 app.get('food-types/:type', function (request, response) {
   var type = request.params.type;
-  var truckList = trucks.filterTrucksByFoodType(typr);
+  var truckList = trucks.filterTrucksByFoodType(type);
 
   response.send(truckList);
-})
+});
 
 
 app.listen(3000, function() {
